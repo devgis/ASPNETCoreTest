d97c42dbff7cb8870a6c03984549920697c11130
devgis <devgis@163.com>
2019-05-21 23:53:53 -0700

    V1
    V1

---
+++ Code/NETCoreAPPTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace NETCoreAPPTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string constr = "Data Source = 192.168.0.200;Initial Catalog = test;User Id = sa;Password = 123456;";
+            SqlConnection con = new SqlConnection(constr);
+            string sql = "SELECT * FROM t_person";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            //var fac = new System.Data.SqlClient.SqlClientFactory();
+            
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["name"].ToString());
+            }
+            con.Close();
+
+
+            Console.ReadLine();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
