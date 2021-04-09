a67448bc9182999dcb15d25e9a1252349c0d7aec
devgis <devgis@163.com>
2021-04-09 12:07:10 +0800

    Configure OK
    Configure OK

--- Code/CoreMVCWebApp/MiddleWares/RequestIPMiddleware.cs
+++ Code/CoreMVCWebApp/MiddleWares/RequestIPMiddleware.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -22,6 +24,13 @@ namespace CoreMVCWebApp.MiddleWares
         public async Task Invoke(HttpContext context)
         {
             _logger.LogInformation($"User IP:{context.Connection.RemoteIpAddress.ToString()}");
+
+            //ReadConfig
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("myconfig.json"); 
+            var config = builder.Build();
+            _logger.LogInformation($"Hello Configure:{config["Myconfig:Hello"]}");
             await _next.Invoke(context);
         }
     }
